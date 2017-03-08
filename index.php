26f785f20de49acee2000e7f0aafc7dd152c842e
baggerest <baggerest@yahoo.com.tw>
2017-03-08 09:13:30 +0800

    新增函式

--- index.php
+++ index.php
@@ -34,6 +34,25 @@ function get_itemdb_list($input_itemdb_file_path){
     return $itemdb_list;
 }
 
+function get_idnum2itemdisplaynametable_list($input_idnum2itemdisplaynametable_file_path){
+    $idnum2itemdisplaynametable_cht_list_output = null;
+    $idnum2itemdisplaynametable = file($input_idnum2itemdisplaynametable_file_path,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+    $idnum2itemdisplaynametable_list = null;
+    foreach ($idnum2itemdisplaynametable as $value){
+        if(strpos($value,'//')===0){
+            continue;
+        }else{
+            $substr_list = substr($value,0,-1);
+            $idnum2itemdisplaynametable_cht_list_output[] = explode('#',$substr_list);
+        }
+    }
+    // return array
+    return $idnum2itemdisplaynametable_cht_list_output;
+}
+
+$tag_itemdb = get_itemdb_list("D:\\GitHub\\rathena\\db\\re\\item_db.txt");
+$language_idnum2itemdisplaynametable = get_idnum2itemdisplaynametable_list("D:\\downloaded\\data\\idnum2itemdisplaynametable.txt");
+
 function from_itemdb($input_itemdb_list = array(),$input_itemdb_language = array()){
     error_reporting(0);
     $itemdb_cht_list_output = null;
@@ -57,26 +76,7 @@ function from_itemdb($input_itemdb_list = array(),$input_itemdb_language = array
     return $itemdb_cht_list_output;
 }
 
-function get_idnum2itemdisplaynametable_list($input_idnum2itemdisplaynametable_file_path){
-    $idnum2itemdisplaynametable = file($input_idnum2itemdisplaynametable_file_path,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-    $idnum2itemdisplaynametable_list = null;
-    foreach ($idnum2itemdisplaynametable as $value){
-        if(strpos($value,'//')===0){
-            continue;
-        }else{
-
-        }
-    }
-
-
-
-    // return array
-}
-
-$tag_itemdb = get_itemdb_list("D:\\GitHub\\rathena\\db\\re\\item_db.txt");
-$idnum2itemdisplaynametable_language = get_idnum2itemdisplaynametable_list("D:\\downloaded\\data\\idnum2itemdisplaynametable.txt");
-
-function from_idnum2itemdisplaynametable($tag_itemdb = array(),$idnum2itemdisplaynametable_language = array()){
+function from_idnum2itemdisplaynametable($tag_itemdb = array(),$input_idnum2itemdisplaynametable_language = array()){
 
     // return array
 }
