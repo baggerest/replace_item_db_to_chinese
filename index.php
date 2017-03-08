0d0f8477d78b894cb248dea56f978bd3c4265a86
baggerest <baggerest@yahoo.com.tw>
2017-03-08 11:54:16 +0800

    新增函式

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
 header("Content-Type:text/html;charset=big5");
-/*$tag_itemdb = get_itemdb_list("D:\\GitHub\\rathena\\db\\re\\item_db.txt");
-$language_itemdb = get_itemdb_list("D:\\downloaded\\rAthenaCN_cht\\db\\re\\item_db.txt");
-foreach (from_itemdb($tag_itemdb,$language_itemdb) as $i => $item){
+//$tag_itemdb = get_itemdb_list("D:\\GitHub\\rathena\\db\\re\\item_db.txt");
+//$language_itemdb = get_itemdb_list("D:\\downloaded\\rAthenaCN_cht\\db\\re\\item_db.txt");
+
+//$tag_itemdb = get_itemdb_list("D:\\GitHub\\rathena\\db\\re\\item_db.txt");
+//$language_idnum2itemdisplaynametable = get_idnum2itemdisplaynametable_list("D:\\downloaded\\data\\idnum2itemdisplaynametable.txt");
+
+/*
+foreach (from_idnum2itemdisplaynametable($tag_itemdb,$language_idnum2itemdisplaynametable) as $i => $item){
     foreach ($item as $j => $value){
+        if($j===0)echo $a++.")";
         echo ($j<19)?"{$value}":"{{$value}}";
         echo ($j<21)?",":"";
     }
     echo "<br>";
-}*/
+}
+*/
 
 function get_itemdb_list($input_itemdb_file_path){
     // 22 counts:1 array
@@ -35,48 +42,62 @@ function get_itemdb_list($input_itemdb_file_path){
 }
 
 function get_idnum2itemdisplaynametable_list($input_idnum2itemdisplaynametable_file_path){
-    $idnum2itemdisplaynametable_cht_list_output = null;
+    $idnum2itemdisplaynametable_changed_list_output = null;
     $idnum2itemdisplaynametable = file($input_idnum2itemdisplaynametable_file_path,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-    $idnum2itemdisplaynametable_list = null;
     foreach ($idnum2itemdisplaynametable as $value){
         if(strpos($value,'//')===0){
             continue;
         }else{
             $substr_list = substr($value,0,-1);
-            $idnum2itemdisplaynametable_cht_list_output[] = explode('#',$substr_list);
+            $idnum2itemdisplaynametable_changed_list_output[] = explode('#',$substr_list);
         }
     }
     // return array
-    return $idnum2itemdisplaynametable_cht_list_output;
+    return $idnum2itemdisplaynametable_changed_list_output;
 }
 
-$tag_itemdb = get_itemdb_list("D:\\GitHub\\rathena\\db\\re\\item_db.txt");
-$language_idnum2itemdisplaynametable = get_idnum2itemdisplaynametable_list("D:\\downloaded\\data\\idnum2itemdisplaynametable.txt");
-
-function from_itemdb($input_itemdb_list = array(),$input_itemdb_language = array()){
+function from_itemdb($input_itemdb_list = array(),$input_itemdb_language_list = array()){
     error_reporting(0);
-    $itemdb_cht_list_output = null;
+    $itemdb_changed_list_output = null;
     foreach($input_itemdb_list as $id => $value){
-        if($input_itemdb_language[$id]===null){
-            $itemdb_cht_list_output[$id] = $input_itemdb_list[$id];
+        if($input_itemdb_language_list[$id]===null){
+            $itemdb_changed_list_output[$id] = $input_itemdb_list[$id];
         }else {
             foreach ($input_itemdb_list[$id] as $key => $item) {
                 switch ($key) {
                     case 2:
-                        $itemdb_cht_list_output[$id][$key] = $input_itemdb_language[$id][$key];
+                        $itemdb_changed_list_output[$id][$key] = $input_itemdb_language_list[$id][$key];
                         break;
                     default:
-                        $itemdb_cht_list_output[$id][$key] = $input_itemdb_list[$id][$key];
+                        $itemdb_changed_list_output[$id][$key] = $input_itemdb_list[$id][$key];
                         break;
                 }
             }
         }
     }
     // return array
-    return $itemdb_cht_list_output;
+    return $itemdb_changed_list_output;
 }
 
-function from_idnum2itemdisplaynametable($tag_itemdb = array(),$input_idnum2itemdisplaynametable_language = array()){
-
+function from_idnum2itemdisplaynametable($input_itemdb_list = array(),$input_idnum2itemdisplaynametable_language_list = array()){
+    error_reporting(0);
+    $itemdb_changed_list_output = null;
+    foreach ($input_itemdb_list as $key => $value){
+        if($input_idnum2itemdisplaynametable_language_list[$key]===null){
+            $itemdb_changed_list_output[$key] = $input_itemdb_list[$key];
+        }else{
+            foreach ($input_itemdb_list[$key] as $id => $item){
+                switch ($id) {
+                    case 2:
+                        $itemdb_changed_list_output[$key][$id] = $input_idnum2itemdisplaynametable_language_list[$key][1];
+                        break;
+                    default:
+                        $itemdb_changed_list_output[$key][$id] = $input_itemdb_list[$key][$id];
+                        break;
+                }
+            }
+        }
+    }
     // return array
+    return $itemdb_changed_list_output;
 }
