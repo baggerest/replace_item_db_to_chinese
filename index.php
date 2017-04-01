ef423eccecd2eee5d295273020ba2fde6dc1cb01
baggerest <gg721115>
2017-04-02 02:12:26 +0800

    add mobdb

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ function get_itemdb_list($input_itemdb_file_path){
         if(strpos($line,'//')===0){
             continue;
         }else{
-            $line = mb_convert_encoding($line,'utf-8','auto');
+            $line = mb_convert_encoding($line,'utf-8','utf-8,big5,gb2312');
             $p_pre = substr($line,0,strpos($line,',{'));
             $list_id = explode(',',$p_pre)[0];
             $itemdb_list[$list_id] = explode(',',$p_pre);
@@ -50,26 +50,15 @@ function get_idnum2itemdisplaynametable_list($input_idnum2itemdisplaynametable_f
         if(strpos($value,'//')===0){
             continue;
         }else{
-            $value = mb_convert_encoding($value,'utf-8','auto');
+            $value = mb_convert_encoding($value,'utf-8','utf-8,big5,gb2312');
             $substr_list = substr($value,0,-1);
-            $idnum2itemdisplaynametable_changed_list_output[] = explode('#',$substr_list);
+            $idnum2itemdisplaynametable_changed_list_output[explode('#',$substr_list)[0]] = explode('#',$substr_list);
         }
     }
     // return array[][1]
     return $idnum2itemdisplaynametable_changed_list_output;
 }
 
-
-//$tag_itemdb = get_itemdb_list("D:\\GitHub\\rathena\\db\\re\\item_db.txt");
-//$language_itemdb = get_itemdb_list("D:\\downloaded\\rAthenaCN_cht\\db\\re\\item_db.txt");
-//foreach (from_itemdb($tag_itemdb,$language_itemdb) as $i => $item){
-//    foreach ($item as $j => $value){
-//        if($j===0)echo $a++.")";
-//        echo ($j<19)?"{$value}":"{{$value}}";
-//        echo ($j<21)?",":"";
-//    }
-//    echo "<br>";
-//}
 function from_itemdb($input_itemdb_list = array(),$input_itemdb_language_list = array()){
     error_reporting(0);
     $itemdb_changed_list_output = null;
@@ -93,16 +82,6 @@ function from_itemdb($input_itemdb_list = array(),$input_itemdb_language_list = 
     return $itemdb_changed_list_output;
 }
 
-//$tag_itemdb = get_itemdb_list("D:\\GitHub\\rathena\\db\\re\\item_db.txt");
-//$language_idnum2itemdisplaynametable = get_idnum2itemdisplaynametable_list("D:\\downloaded\\data\\idnum2itemdisplaynametable.txt");
-//foreach (from_idnum2itemdisplaynametable($tag_itemdb,$language_idnum2itemdisplaynametable) as $i => $item){
-//    foreach ($item as $j => $value){
-//        if($j===0)echo $a++.")";
-//        echo ($j<19)?"{$value}":"{{$value}}";
-//        echo ($j<21)?",":"";
-//    }
-//    echo "<br>";
-//}
 function from_idnum2itemdisplaynametable($input_itemdb_list = array(),$input_idnum2itemdisplaynametable_language_list = array()){
     error_reporting(0);
     $itemdb_changed_list_output = null;
@@ -142,3 +121,99 @@ function output_itemdb_file($input_item_db = array(),$output_path = ""){
     fclose($save_file);
     return true;
 }
+//output_itemdb_file(from_idnum2itemdisplaynametable(get_itemdb_list("C:\\my\\rathena\\db\\re\\item_db.txt"),get_idnum2itemdisplaynametable_list("D:\\OpenKoreTW20170328\\tables\\twRO\\items.txt")),"D:\\item_db.txt");
+
+function get_mobdb_list($input_mobdb_file_path){
+    //56 counts:1 array
+    $mobdb = file($input_mobdb_file_path,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+    $mobdb_list = array();
+    foreach ($mobdb as $line){
+        if(strpos($line,'//')===0) {
+            continue;
+        }else{
+            $line = mb_convert_encoding($line,'utf-8','utf-8,big5,gb2312');
+            $mobdb_list[explode(',',$line)[0]] = explode(',',$line);
+        }
+    }
+    //return array[57]
+    return $mobdb_list;
+}
+
+function get_monsters_list($input_monsters_file_path){
+    $monsters_list = array();
+    $monsters = file($input_monsters_file_path,FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+    foreach ($monsters as $line){
+        if(strpos($line,'//')===0) {
+            continue;
+        }else{
+            $line = mb_convert_encoding($line,'utf-8','utf-8,big5,gb2312');
+            $monsters_list[explode(' ',$line)[0]] = explode(' ',$line);
+        }
+    }
+    //return array[2]
+    return $monsters_list;
+}
+
+function from_mobdb($input_mobdb_list = array(),$input_mobdb_language_list = array()){
+    error_reporting(0);
+    $mobdb_changed_list_output = null;
+    foreach($input_mobdb_list as $id => $value){
+        if($input_mobdb_language_list[$id]===null){
+            $mobdb_changed_list_output[$id] = $input_mobdb_list[$id];
+        }else {
+            foreach ($input_mobdb_list[$id] as $key => $item) {
+                switch ($key) {
+                    case 2:
+                        $mobdb_changed_list_output[$id][$key] = $input_mobdb_language_list[$id][$key];
+                        break;
+                    default:
+                        $mobdb_changed_list_output[$id][$key] = $input_mobdb_list[$id][$key];
+                        break;
+                }
+            }
+        }
+    }
+    // return array[][57]
+    return $mobdb_changed_list_output;
+}
+
+function from_monsters($input_mobdb_list = array(),$input_monsters_language_list = array()){
+    error_reporting(0);
+    $monsters_changed_list_output = null;
+    foreach ($input_mobdb_list as $key => $value){
+        if($input_monsters_language_list[$key]===null){
+            $monsters_changed_list_output[$key] = $input_mobdb_list[$key];
+        }else{
+            foreach ($input_mobdb_list[$key] as $id => $item){
+                switch ($id) {
+                    case 2:
+                        $monsters_changed_list_output[$key][$id] = $input_monsters_language_list[$key][1];
+                        break;
+                    default:
+                        $monsters_changed_list_output[$key][$id] = $input_mobdb_list[$key][$id];
+                        break;
+                }
+            }
+        }
+    }
+    // return array[][21]
+    return $monsters_changed_list_output;
+}
+
+// output utf8 frist
+function output_mobdb_file($input_mob_db = array(),$output_path = ""){
+    $save_file = fopen($output_path,'w');
+    $title = "// ID,Sprite_Name,kROName,iROName,LV,HP,SP,EXP,JEXP,Range1,ATK1,ATK2,DEF,MDEF,STR,AGI,VIT,INT,DEX,LUK,Range2,Range3,Scale,Race,Element,Mode,Speed,aDelay,aMotion,dMotion,MEXP,MVP1id,MVP1per,MVP2id,MVP2per,MVP3id,MVP3per,Drop1id,Drop1per,Drop2id,Drop2per,Drop3id,Drop3per,Drop4id,Drop4per,Drop5id,Drop5per,Drop6id,Drop6per,Drop7id,Drop7per,Drop8id,Drop8per,Drop9id,Drop9per,DropCardid,DropCardper\r\n";
+    fwrite($save_file,$title);
+    foreach ($input_mob_db as $id => $item){
+        $line_words = null;
+        foreach ($input_mob_db[$id] as $key => $value){
+            $line_words .= $value;
+            $line_words .= ($key<56)?",":"\r\n";
+        }
+        fwrite($save_file,$line_words);
+    }
+    fclose($save_file);
+    return true;
+}
+//output_mobdb_file(from_monsters(get_mobdb_list("C:\\my\\rathena\\db\\re\\mob_db.txt"),get_monsters_list("D:\\OpenKoreTW20170328\\tables\\twRO\\monsters.txt")),"D:\\mob_db.txt");
